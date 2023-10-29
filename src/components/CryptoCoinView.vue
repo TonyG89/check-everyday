b5c74621ebde920e3dede259bd9c9f6b30ef28ea
Tony-tuf15 <a.gryhoryev@gmail.com>
2023-10-29 23:27:57 +0200

    crypto data

--- src/components/CryptoCoinView.vue
+++ src/components/CryptoCoinView.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
     <h2>crypto</h2>
-    {{ cryptoData?.symbol }}: {{ cryptoData?.openPrice }}
+    <div v-for="i in sortData" :key="i.symbol">
+      {{ i?.symbol }}: {{ String(i?.openPrice).slice(0, String(i?.openPrice).indexOf('.') + 3) }} (<span
+        :class="i?.priceChange > 0 ? 'text-green-900' : 'text-red-900'"
+      >
+        {{ String(i?.priceChange).slice(0, String(i?.priceChange).indexOf('.') + 3) }}
+      </span>
+      )
+    </div>
   </div>
 </template>
 
@@ -18,18 +25,23 @@ const params = '';
 
 // 'https://api.binance.com/api/v3/exchangeInfo?symbols=["BTCUSDT","BNBBTC"]'
 
-const cryptoData = ref(null);
+const cryptoData = ref([]);
+
+const sortData = computed (() => cryptoData.value.sort((a, b) => b.openPrice - a.openPrice));
 
 const fetchData = async (symbol) => {
   // debugger
   const res = await fetch(`${binance[0]}/${path}?symbol=${symbol}`);
   const data = await res.json();
-  cryptoData.value = data;
+  cryptoData.value = [...cryptoData.value, data];
   return urlData;
 };
 
 onMounted(() => {
   fetchData('BTCUSDT');
+  fetchData('USDTUAH');
+  fetchData('ETHUSDT');
+  fetchData('ETHUAH');
 });
 </script>
 
